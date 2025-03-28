26b5814c1b27eac87c1b97d05770c17fd4ac5be9
Tuan Khang <khangletuan098@gmail.com>
2025-03-28 22:55:22 +0700

    init

---
+++ cicd/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello, World!</h1>
+        <p>This is a simple React app.</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
